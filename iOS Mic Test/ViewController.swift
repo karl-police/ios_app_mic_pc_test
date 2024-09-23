c48dd6701fa482b7d13f8042891248852bdb538c
karl-police <karl-police2001@bluewin.ch>
2024-09-23 21:02:17 +0200

    Update ViewController.swift

--- iOS Mic Test/ViewController.swift
+++ iOS Mic Test/ViewController.swift
@@ -76,14 +76,14 @@ public func GetAvailableMicrophoneInputs() -> [AVAudioSessionPortDescription]? {
 
 
 struct AudioSettings {
-    let formatID: AVAudioFormatID = Int(kAudioFormatAppleLossless)
+    let formatID: AVAudioFormatID = kAudioFormatAppleLossless
     let sampleRate: Double = 44100
     let channels: Int = 1
     let qualityEnconder: AVAudioQuality = AVAudioQuality.high.rawValue
 
     func getForSettings() {
         return [
-            AVFormatIDKey: formatID,
+            AVFormatIDKey: Int(formatID),
             AVSampleRateKey: sampleRate,
             AVNumberOfChannelsKey: channels,
             AVEncoderAudioQualityKey: qualityEnconder.rawValue
