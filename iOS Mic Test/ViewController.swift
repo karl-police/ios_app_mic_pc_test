949312f64976c8107f86ceb4977a0ecf1598e79b
karl-police <karl-police2001@bluewin.ch>
2024-09-26 15:40:45 +0200

    Update ViewController.swift

--- iOS Mic Test/ViewController.swift
+++ iOS Mic Test/ViewController.swift
@@ -323,7 +323,7 @@ class TCPServer {
     init(port: UInt16) {
         // Port Constructor takes UInt16
         self.port = NWEndpoint.Port(rawValue: port)
-        setupTCPServer()
+        startServer()
     }
 
     func startServer() throws {
