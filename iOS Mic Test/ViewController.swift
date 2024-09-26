64850449ba42f1e574a9af401e070f9dfc14b561
karl-police <karl-police2001@bluewin.ch>
2024-09-26 16:53:46 +0200

    Update ViewController.swift

--- iOS Mic Test/ViewController.swift
+++ iOS Mic Test/ViewController.swift
@@ -385,8 +385,8 @@ class NetworkVoiceManager {
     var default_port: UInt16 = 8125
     var audioEngineManager: AudioEngineManager!
 
-    init(withAudioEngineManager: audioEngineManager) {
-        self.audioEngineManager = audioEngineManager
+    init(withAudioEngineManager: AudioEngineManager) {
+        self.audioEngineManager = withAudioEngineManager
 
         self.tcpServer = TCPServer(inputPort: default_port)
     }
