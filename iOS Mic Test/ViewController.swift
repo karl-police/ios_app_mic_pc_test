2578ff31d53f95662a663c6d8ceb4af4ffb3dfde
karl-police <karl-police2001@bluewin.ch>
2024-09-27 06:38:55 +0200

    Update ViewController.swift

--- iOS Mic Test/ViewController.swift
+++ iOS Mic Test/ViewController.swift
@@ -516,7 +516,7 @@ class NetworkVoiceManager {
     }
 
     func transmitAudio(buffer: AVAudioPCMBuffer, _ connection: NWConnection) {
-        let audioData = buffer.audioBufferList.pointee.mBuffers
+        /*let audioData = buffer.audioBufferList.pointee.mBuffers
         let dataSize = audioData.mDataByteSize
         
         // Check if data is available
@@ -536,7 +536,7 @@ class NetworkVoiceManager {
                     G_UI_Class_connectionLabel.setStatusConnectionText("Error sending audio data: \(error)")
                 }
             })
-        )
+        )*/
     }
 
 
