d8297fbb7ce6f75152165c69d52224ab8b02577b
karl-police <karl-police2001@bluewin.ch>
2024-09-30 19:34:50 +0200

    Network Dump

--- iOS Mic Test/Include/TCPServer.swift
+++ iOS Mic Test/Include/TCPServer.swift
@@ -44,43 +44,9 @@ struct Utils_NWDump {
                     + "disableFragmentation: \(options.disableFragmentation)\n"
                     + "disableMulticastLoopback: \(options.disableMulticastLoopback)\n"
                     + "localAddressPreference: \(options.localAddressPreference)\n"
+        } else {
+            debugText = "NOT HANDLED\n"
         }
-
-        /*switch nwProtocolOptions {
-            case let options = nwProtocolOptions as? NWProtocolTCP.Options:
-                debugText += "enableFastOpen: \(options.enableFastOpen)\n"
-                    + "maximumSegmentSize: \(options.maximumSegmentSize)\n"
-                    + "noDelay: \(options.noDelay)\n"
-                    + "noOptions: \(options.noOptions)\n"
-                    + "noPush: \(options.noPush)\n"
-                    + "retransmitFinDrop: \(options.retransmitFinDrop)\n"
-                    + "disableAckStretching: \(options.disableAckStretching)\n"
-                    + "disableECN: \(options.disableECN)\n"
-                    + "\n"
-                    + "enableKeepalive: \(options.enableKeepalive)\n"
-                    + "keepaliveIdle: \(options.keepaliveIdle)\n"
-                    + "keepaliveCount: \(options.keepaliveCount)\n"
-                    + "keepaliveInterval: \(options.keepaliveInterval)\n"
-                    + "\n"
-                    + "connectionTimeout: \(options.connectionTimeout)\n"
-                    + "connectionDropTime: \(options.connectionDropTime)\n"
-                    + "persistTimeout: \(options.persistTimeout)\n"
-
-            case let options = nwProtocolOptions as? NWProtocolUDP.Options:
-                debugText += "preferNoChecksum: \(options.preferNoChecksum)\n"
-
-            case let options = nwProtocolOptions as? NWProtocolIP.Options:
-                debugText += "version: \(options.version)\n"
-                    + "shouldCalculateReceiveTime: \(options.shouldCalculateReceiveTime)\n"
-                    + "hopLimit: \(options.hopLimit)\n"
-                    + "useMinimumMTU: \(options.useMinimumMTU)\n"
-                    + "disableFragmentation: \(options.disableFragmentation)\n"
-                    + "disableMulticastLoopback: \(options.disableMulticastLoopback)\n"
-                    + "localAddressPreference: \(options.localAddressPreference)\n"
-
-            default:
-                debugText = "NOT DEFINED\n"
-        }*/
 
         return debugText
     }
