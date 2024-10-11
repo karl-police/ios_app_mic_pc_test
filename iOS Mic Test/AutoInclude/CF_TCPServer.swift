6c4e60e1a52a95d23061660a4538589220a53d09
karl-police <karl-police2001@bluewin.ch>
2024-10-11 17:16:57 +0200

    Update CF_TCPServer.swift

--- iOS Mic Test/AutoInclude/CF_TCPServer.swift
+++ iOS Mic Test/AutoInclude/CF_TCPServer.swift
@@ -282,7 +282,7 @@ class CF_TCPServer {
         CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .defaultMode)
         self.OnServerStateChanged(CF_ServerStates.started)
 
-        DispatchQueue.global(qos: .userInitiated).async {
+        DispatchQueue.global(qos: .background).async {
             CFRunLoopRun() // Run server loop
         }
     }
