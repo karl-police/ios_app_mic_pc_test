95f648b72005af670408ff0c22bacbf4f3186197
karl-police <karl-police2001@bluewin.ch>
2024-10-23 01:36:43 +0200

    Update CF_TCPServer.swift

--- iOS Mic Test/AutoInclude/CF_TCPServer.swift
+++ iOS Mic Test/AutoInclude/CF_TCPServer.swift
@@ -313,13 +313,14 @@ class CF_NetworkServer {
         semaphore.signal() // Signal
     }
     func WaitForData(from expectAddr: CFData) -> Data? {
-        // yep
+        // yep idk
+        // it doesn't work properly
 
-        if (self.receivedUDPData == nil) {
-            self.TemporaryLogging("receivedUDPData is nil")
-            //semaphore.wait()
-        }
-        semaphore.wait()
+        /*if (self.receivedUDPData == nil) {
+            self.TemporaryLogging("waiting for data")
+            semaphore.wait()
+        }*/
+        semaphore.wait(timeout: .now() + 5)
 
         guard let receivedUDP_lastFromAddr = self.receivedUDP_lastFromAddr else {
             self.TemporaryLogging("no address")
@@ -420,8 +421,6 @@ class CF_NetworkServer {
 
                     if let dataPointer = dataPointer {
                         let data = Unmanaged<CFData>.fromOpaque(dataPointer).takeUnretainedValue() as Data
-                        
-                        // No clue why, but async here fixes the event thing
                         DispatchQueue.main.async {
                             referencedSelf.OnServerDataReceived(data, from: address)
                         }
